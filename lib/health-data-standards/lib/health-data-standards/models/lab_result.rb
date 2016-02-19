ea2d030a936a57d51294883620ad9fe853d21b72
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-02-19 09:51:05 -0600

    Updated reference to HDS gem (instead of local copy)

--- lib/health-data-standards/lib/health-data-standards/models/lab_result.rb
+++
@@ -1,8 +0,0 @@
-class LabResult < Entry
-  field :referenceRange, as: :reference_range, type: String
-  field :interpretation, type: Hash  
-  field :reaction,   type: Hash
-  field :method,  type: Hash
-
-  
-end
